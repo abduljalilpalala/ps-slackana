96ccfeb6e2b062113d9c92d7d5dc569aa263d7d7
Joshua Galit <108642414+jsvelte@users.noreply.github.com>
2022-09-26 11:20:45 +0800

    [Markup]-05 User Sign In Page (#3)
    Co-authored-by: acatzk <joshuaimalay@gmail.com>

--- client/src/shared/twin/globals.styles.ts
+++ client/src/shared/twin/globals.styles.ts
@@ -11,6 +11,7 @@ export const globals = {
     tw`block w-full rounded border-none px-3 py-2`,
     tw`text-sm placeholder:text-[#94a3b8] text-[#0f172a]`,
     tw`ring-1 ring-[#cbd5e1] transition duration-150 ease-in-out hover:ring-2`,
-    tw`hover:ring-blue-600 focus:ring-2 focus:ring-blue-600 disabled:opacity-50`
+    tw`hover:ring-blue-600 focus:ring-2 focus:ring-blue-600 disabled:opacity-50`,
+    tw`disabled:hover:ring-2`
   ]
 }
--- client/src/shared/validation/index.ts
+++ client/src/shared/validation/index.ts
@@ -1,11 +1,16 @@
 import * as Yup from 'yup'
 
+export const SignInFormSchema = Yup.object().shape({
+  email: Yup.string().email().required().label('Email'),
+  password: Yup.string().required('Password is required')
+})
+
 export const SignUpFormSchema = Yup.object().shape({
   name: Yup.string()
     .required('Name is required')
     .min(2, 'Should have atleast 2 characters')
     .max(30, 'Should have max length of 30 characters'),
-  email: Yup.string().email('Email must be valid email').required('Email is required'),
+  email: Yup.string().email().required().label('Email'),
   password: Yup.string()
     .required('Password is required')
     .min(4, 'Password length should be at least 4 characters')
