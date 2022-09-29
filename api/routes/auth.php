ad3f0e7187299756dcc6484dcb64c62c3c8f1ac7
Abdul Jalil Palala <108660012+abduljalilpalala@users.noreply.github.com>
2022-09-29 12:59:53 +0800

    [BE]-48 Fix Sign Up Controller (#13)

--- api/routes/auth.php
+++ api/routes/auth.php
@@ -1,11 +1,11 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
-use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\SignUpController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/sign-in', [AuthController::class, 'store']);
-Route::post('/sign-up', [RegisterController::class, 'store']);
+Route::post('/sign-up', [SignUpController::class, 'store']);
 
 Route::group(['middleware' => 'auth:sanctum'], function() {
   Route::get('/auth', [AuthController::class, 'index']);
