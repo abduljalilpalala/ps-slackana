fa9ef1af5841b8f5075ee1e6cedef002a4c00e18
John Paul Banera <johnpaul.banera@sun-asterisk.com>
2022-10-09 22:03:28 +0800

    [FE]-37 Integrate Set Status Functionality

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,47 +1,99 @@
 import { NextPage } from 'next'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
+import {
+  createProject,
+  filterProjects,
+  setProjectTitle,
+  setProjectDescription,
+} from '~/redux/project/projectSlice'
+import getDate from '~/utils/getDate'
 import { Add } from '~/shared/icons/AddIcon'
+import getGreetings from '~/utils/getGreetings'
 import { styles } from '~/shared/twin/auth.styles'
 import { Spinner } from '~/shared/icons/SpinnerIcon'
 import { globals } from '~/shared/twin/globals.styles'
-import Layout from '~/components/templates/HomeLayout'
 import { ThreeDot } from '~/shared/icons/ThreeDotIcon'
-import DropDown from '~/components/organisms/DropDownFilter'
+import Layout from '~/components/templates/HomeLayout'
 import InputTags from '~/components/molecules/InputTags'
 import DialogBox from '~/components/templates/DialogBox'
-import { projectDataList } from '~/shared/jsons/projectDataList'
+import DropDown from '~/components/organisms/DropDownFilter'
 import ProjectTemplate from '~/components/templates/ProjectTemplate'
 import { styles as homeStyle } from '~/shared/twin/home-content.style'
-import { useAppSelector } from '~/hooks/reduxSelector'
+import { useAppSelector, useAppDispatch } from '~/hooks/reduxSelector'
+import SeeMore from '~/components/atoms/SeeMore'
+import SubmitButton from '~/components/atoms/SubmitButton'
 
 const Index: NextPage = (): JSX.Element => {
+  const dispatch = useAppDispatch();
   const [limit, setLimit] = useState<boolean>(false)
-  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
   const [newProjectModal, setNewProjectModal] = useState<boolean>(false)
-  const { user } = useAppSelector((state) => state.auth)
-  const { name } = user || {}
 
-  const projectList = projectDataList
-    .slice(0, limit ? projectDataList.length : 12)
-    .map((data: any, index: number) => {
-      return <ProjectTemplate data={data} key={index} />
-    })
+  const {
+    auth: { user },
+    project: { filter, project, isLoading, newProject, } } = useAppSelector((state) => state);
+
+  const { name } = user || {};
+  const { title, description } = newProject || {};
+
+  useEffect(() => {
+    if (isLoading) {
+      setLimit(false);
+      dispatch(filterProjects());
+    }
+  }, [filter, isLoading])
+
+  const onCreateProject = (): void => {
+    setNewProjectModal(false);
+    dispatch(createProject(newProject));
+  }
+
+  const [isTitleDisabled, setIsTitleDisabled] = useState<boolean>(true);
+  const [isDescriptionDisabled, setIsDescriptionDisabled] = useState<boolean>(true);
+  const onChange = (e: any): void => {
+    const value = e.target.value;
+    const name = e.target.name;
+
+    if (name === "title") {
+      dispatch(setProjectTitle(value));
+      if (value.length === 0) {
+        setIsTitleDisabled(true);
+      } else {
+        setIsTitleDisabled(false);
+      }
+    } else {
+      dispatch(setProjectDescription(value));
+      if (value.length === 0) {
+        setIsDescriptionDisabled(true);
+      } else {
+        setIsDescriptionDisabled(false);
+      }
+    }
+  }
+
+  const projectList = isLoading
+    ? <ProjectTemplate data={null} isLoading={isLoading} />
+    : project?.slice(0, limit ? project?.length : 12)
+      .map((data: any, index: number) => {
+        return <ProjectTemplate data={data} key={index} isLoading={isLoading} />
+      })
 
   const addNewProject = (
     <DialogBox isOpen={true} closeModal={() => setNewProjectModal(false)} headerTitle="New project">
 
       <div className='flex flex-col gap-9'>
         <div >
-          <label htmlFor="tite" css={globals.form_label} className="float-left">
+          <label htmlFor="title" css={globals.form_label} className="float-left">
             Title <span>*</span>
           </label>
           <input
             type="text"
-            name="tite"
+            name="title"
             css={globals.form_control}
-            disabled={isSubmitting}
+            disabled={isLoading}
             placeholder="Project name"
+            value={title}
+            onChange={onChange}
           />
         </div>
 
@@ -49,7 +101,7 @@ const Index: NextPage = (): JSX.Element => {
           <p css={globals.form_label} className="">
             Team <span>*</span>
           </p>
-          <InputTags isSubmitting={isSubmitting} />
+          <InputTags isSubmitting={isLoading} type="create" />
         </div>
 
         <div>
@@ -59,32 +111,23 @@ const Index: NextPage = (): JSX.Element => {
           <textarea
             name="description"
             css={globals.form_control}
-            disabled={isSubmitting}
+            disabled={isLoading}
             placeholder="Lorem ipsum dolor sit amet, consectetur."
             rows={6}
+            value={description}
+            onChange={onChange}
           />
         </div>
+        <SubmitButton
+          text="Save"
+          submitted={onCreateProject}
+          isSubmitting={isLoading}
+          isDisabled={isDescriptionDisabled || isTitleDisabled}
+          className={`!text-slate-50 !bg-blue-600 hover:!bg-blue-600 opacity-60 hover:!opacity-100 ${(isDescriptionDisabled || isTitleDisabled) && "hover:!opacity-60 !cursor-not-allowed"}`} />
 
-        <button
-          onClick={() => setIsSubmitting(!isSubmitting)}
-          type="submit"
-          css={styles.form_submit}
-          disabled={isSubmitting}
-        >
-          {isSubmitting ? <Spinner className="h-5 w-5" /> : 'Save'}
-        </button>
       </div>
     </DialogBox>
   )
-
-  const add = () => {
-    console.log('add')
-    setNewProjectModal(true)
-  }
-
-  const filter = (value: string) => {
-    console.log(value)
-  }
 
   return (
     <Layout metaTitle="Home">
@@ -92,8 +135,8 @@ const Index: NextPage = (): JSX.Element => {
       <div className="default-scrollbar grid w-full h-screen overflow-y-scroll p-10">
         <div className="!max-w-[900px] flex flex-col items-center justify-center gap-16 md:container mobile:!pb-20 mobile:!gap-10">
           <div className="flex flex-col items-center justify-center gap-5">
-            <p className="text-sm">Tuesday, September 20</p>
-            <h1 className="text-center text-3xl">Good afternoon, {name}</h1>
+            <p className="text-sm">{getDate('today')}</p>
+            <h1 className="text-center text-3xl">{getGreetings()}, {name}</h1>
           </div>
 
           <div className="w-full">
@@ -101,14 +144,14 @@ const Index: NextPage = (): JSX.Element => {
               <div className="items-between flex flex-row justify-between ">
                 <div className="flex flex-row items-center justify-center gap-3">
                   <div
-                    onClick={() => add()}
+                    onClick={() => setNewProjectModal(true)}
                     className="flex h-12 w-12 cursor-pointer items-center justify-center rounded-lg border border-dotted border-slate-400 hover:bg-slate-100"
                   >
                     <Add />
                   </div>
                   <h1 className="text-2xl">Projects</h1>
                 </div>
-                <DropDown callback={filter}>
+                <DropDown>
                   <div className='hover:bg-slate-100 z-0 cursor-pointer w-12 h-12 flex items-center justify-center rounded-lg border border-slate-500'>
                     <ThreeDot />
                   </div>
@@ -116,12 +159,15 @@ const Index: NextPage = (): JSX.Element => {
               </div>
             </div>
 
-            <div className="grid h-px-400 grid-cols-3 gap-6 overflow-y-scroll rounded-b-lg border-x border-b border-slate-300 px-7 py-10 scrollbar-thin scrollbar-track-gray-100 scrollbar-thumb-slate-400 scrollbar-thumb-rounded-md tablet:grid-cols-2 mobile:grid-cols-1">
+            <div className="grid h-px-400 grid-cols-3 col-span-1	gap-6 overflow-y-scroll rounded-b-lg border-x border-b border-slate-300 px-7 py-10 scrollbar-thin scrollbar-track-gray-100 scrollbar-thumb-slate-400 scrollbar-thumb-rounded-md tablet:grid-cols-2 mobile:grid-cols-1">
               {projectList}
-              {projectDataList.slice(0, 12).length === 12 && (
-                <div onClick={() => setLimit(!limit)} css={homeStyle.more}>
-                  <span>Show {limit ? 'less' : 'more'}</span>
-                </div>
+
+              {project?.length <= 12 && [...Array(12 - project?.length)].map(_ => {
+                return <div data-name="spacer" key={Math.random()}></div>;
+              })}
+
+              {project?.slice(0, 13).length === 13 && (
+                isLoading || <SeeMore set={setLimit} what={limit} />
               )}
             </div>
           </div>
--- client/src/pages/team/[id]/overview.tsx
+++ client/src/pages/team/[id]/overview.tsx
@@ -1,58 +1,97 @@
-import React, { FC, useState } from 'react'
-import { useRouter } from 'next/router'
+import moment from 'moment'
 import { Plus } from 'react-feather'
+import { useRouter } from 'next/router'
+import React, { FC, useEffect, useState } from 'react'
 
-import ProjectLayout from '~/components/templates/ProjectLayout'
-import SubmitButton from '~/components/atoms/SubmitButton'
-import TeamTemplate from '~/components/molecules/TeamTemplate'
-import MembersTemplate from '~/components/molecules/MembersTemplate'
-import { membersData } from '~/shared/jsons/membersData'
-import { teamTempoData } from '~/shared/jsons/teamTempoData'
-import DialogBox from '~/components/templates/DialogBox'
+import {
+  setID,
+  addNewTeam,
+  getProject,
+  renameTeamData,
+  setTeamNewName,
+  setEditProjectID,
+  setEditProjectTitle,
+  updateProjectDetails,
+  setEditProjectDescription,
+  resetRefresher,
+  startRefresher,
+  projectRefresher,
+  teamRefresher,
+} from '~/redux/project/projectSlice'
+import SeeMore from '~/components/atoms/SeeMore'
 import { globals } from '~/shared/twin/globals.styles'
 import InputTags from '~/components/molecules/InputTags'
+import DialogBox from '~/components/templates/DialogBox'
+import SubmitButton from '~/components/atoms/SubmitButton'
+import TeamTemplate from '~/components/molecules/TeamTemplate'
+import ProjectLayout from '~/components/templates/ProjectLayout'
+import MembersTemplate from '~/components/molecules/MembersTemplate'
+import LineSkeleton from '~/components/atoms/Skeletons/LineSkeleton'
+import { useAppDispatch, useAppSelector } from '~/hooks/reduxSelector'
 import { styles as homeStyle } from '~/shared/twin/home-content.style'
 
 const Overview: FC = (): JSX.Element => {
-  const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+  const router = useRouter();
+  const { id } = router.query;
+  const dispatch = useAppDispatch();
+
   const [teamLimit, setTeamLimit] = useState<boolean>(false);
   const [membersLimit, setMembersLimit] = useState<boolean>(false);
 
-  const [addTeamState, setAddTeamState] = useState<boolean>(false);
+  const { overviewProject, renameTeamData: getTeamData, projectDescription, isLoading, refresher } = useAppSelector((state) => state.project);
+  const { title, description, created_at, teams, members } = overviewProject || {};
+  const { teamStateUpdate, projectStateUpdate, memberStateUpdate } = refresher || {};
+  const { title: editTitle, description: editDescription } = projectDescription || {};
+  const { name } = getTeamData || {};
+
+  useEffect(() => {
+    setTeamLimit(false);
+    dispatch(startRefresher());
+    dispatch(setEditProjectID(id));
+    dispatch(setEditProjectTitle(title));
+    dispatch(setEditProjectDescription(description));
+    dispatch(getProject(id))
+      .then(_ => {
+        dispatch(resetRefresher());
+      });
+  }, [id, title])
+
+  const updateTitle = (e: any) => {
+    const value = e.target.value;
+    dispatch(setEditProjectTitle(value));
+  };
+  const updateDescription = (e: any) => {
+    const value = e.target.value;
+    dispatch(setEditProjectDescription(value));
+  };
+  const [saveChanges, setSaveChanges] = useState<boolean>(false);
+  const onSaveChanges = () => {
+    setSaveChanges(true);
+    dispatch(projectRefresher());
+    dispatch(updateProjectDetails())
+      .then(_ => {
+        setSaveChanges(false);
+        dispatch(resetRefresher());
+      });
+  };
+
   const [renameTeamState, setRenameTeamState] = useState<boolean>(false);
-
-  const [renameThis, setRenameThis] = useState<string>("");
-
-  const router = useRouter()
-  const { id } = router.query;
-
-  const addTeam = (): void => {
-    setAddTeamState(!addTeamState);
-  }
-
-  const addTeamSubmit = (): void => {
-    console.log("Integrate add team save");
-    setIsSubmitting(true);
-  }
-
-  const renameTeam = (team?: string): void => {
-    console.log("Integrate rename team for", team);
-    setRenameThis(team || "");
+  const renameTeam = (teamID?: number): void => {
+    dispatch(setID({ projectID: id, teamID }))
     setRenameTeamState(!renameTeamState);
-  }
+  };
+  const renameTeamSubmit = (): void => {
+    setRenameTeamState(!renameTeamState);
 
-  const addTeamModal = (
-    <DialogBox isOpen={addTeamState} closeModal={addTeam} headerTitle="Add team">
-      <div css={homeStyle.tags}>
-        <p css={globals.form_label} className="">
-          Team <span>*</span>
-        </p>
-        <InputTags isSubmitting={false} />
-      </div>
-      <SubmitButton text="Save" submitted={addTeamSubmit} isSubmitting={isSubmitting} className="mobile:!w-full !text-slate-50 !bg-blue-600 hover:!bg-blue-800" />
-    </DialogBox>
-  );
-
+    dispatch(teamRefresher());
+    dispatch(renameTeamData())
+      .then(_ => {
+        dispatch(getProject(id))
+          .then(_ => {
+            dispatch(resetRefresher());
+          })
+      });
+  };
   const renameModal = (
     <DialogBox isOpen={renameTeamState} closeModal={renameTeam} headerTitle="Rename team">
       <div >
@@ -63,15 +102,53 @@ const Overview: FC = (): JSX.Element => {
           type="text"
           name="tite"
           css={globals.form_control}
-          disabled={isSubmitting}
+          disabled={isLoading}
           placeholder="Team name"
-          value={renameThis}
+          value={name}
           onChange={(e: any) => {
-            setRenameThis(e.target.value)
+            dispatch(setTeamNewName(e.target.value))
           }}
         />
       </div>
-      <SubmitButton text="Save" submitted={addTeamSubmit} isSubmitting={isSubmitting} className="mobile:!w-full !text-slate-50 !bg-blue-600 hover:!bg-blue-800" />
+      <SubmitButton text="Save" submitted={renameTeamSubmit} isSubmitting={isLoading} className="mobile:!w-full !text-slate-50 !bg-blue-600 hover:!bg-blue-800" />
+    </DialogBox>
+  );
+
+  const [detectChanges, setDetectChanges] = useState<boolean>(true);
+  const [addTeamState, setAddTeamState] = useState<boolean>(false);
+  const addTeamSubmit = (): void => {
+    setAddTeamState(!addTeamState)
+
+    dispatch(teamRefresher());
+    dispatch(addNewTeam())
+      .then(_ => {
+        dispatch(getProject(id))
+          .then(_ => {
+            dispatch(resetRefresher());
+          })
+      });
+  };
+  const addTeamModal = (
+    <DialogBox isOpen={addTeamState} closeModal={() => setAddTeamState(!addTeamState)} headerTitle="Add team">
+      <div css={homeStyle.tags}>
+        <p css={globals.form_label} className="">
+          Team <span>*</span>
+        </p>
+        <InputTags isSubmitting={isLoading} type="update" changes={setDetectChanges} />
+      </div>
+      <div className='flex gap-3'>
+        <SubmitButton
+          text="Cancel"
+          submitted={() => setAddTeamState(false)}
+          isSubmitting={false}
+          className="!text-slate-900 !bg-slate-300 hover:!bg-blue-600 hover:!text-slate-50" />
+        <SubmitButton
+          text="Save"
+          submitted={addTeamSubmit}
+          isSubmitting={isLoading}
+          isDisabled={detectChanges}
+          className={`!text-slate-50 !bg-blue-600 hover:!bg-blue-600 opacity-60 hover:!opacity-100 ${detectChanges && "hover:!opacity-60 !cursor-not-allowed"}`} />
+      </div>
     </DialogBox>
   );
 
@@ -79,57 +156,80 @@ const Overview: FC = (): JSX.Element => {
     <ProjectLayout metaTitle="Overview">
       {renameModal}
       {addTeamModal}
-      <div className="pb-40 grid h-screen place-items-center w-full px-10 overflow-y-scroll scrollbar-thin scrollbar-track-gray-100 scrollbar-thumb-slate-400 scrollbar-thumb-rounded-md">
-        <div className='py-20 max-w-[900px] flex flex-col gap-10 items-start justify-center'>
-          <div className='flex flex-col gap-6'>
-            <div className='flex justify-between items-end mobile:!flex-wrap'>
-              <h1 className='text-4xl font-bold text-slate-900'>Team 6 Digits</h1>
-              <span className='text-xl font-medium text-slate-900'>September 21, 2022</span>
-            </div>
-            <p className='text-sm text-slate-500'>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur a arcu et tortor dictum finibus sed id odio.
-              Praesent sed massa lectus . Nam suscipit diam non condimentum luctus. Praesent pharetra turpis in ligula faucibus maximus.
-            </p>
-            <div className='flex w-full items-end justify-end'>
-              <SubmitButton text="Save" submitted={() => console.log("save")} isSubmitting={false} className="!w-[175px] mobile:!w-full !text-slate-600 !bg-slate-200 hover:!bg-blue-600 hover:!text-slate-50" />
-            </div>
-          </div>
-
-          <div className='w-full flex flex-col gap-3'>
-            <h1 className='text-lg font-bold'>Team</h1>
-            <div className='grid grid-cols-4 gap-6 tablet:!grid-cols-2 mobile:!grid-cols-1'>
-              <div onClick={addTeam} className='flex items-center gap-2 w-[200px] cursor-pointer'>
-                <div className='w-[44px] h-[44px] flex justify-center items-center rounded-full border-2 border-dotted border-slate-600'>
-                  <Plus />
-                </div>
-                <button className='text-base font-semibold'>Add Team</button>
+      <div className="pb-40 grid h-screen w-full px-10 overflow-y-scroll scrollbar-thin scrollbar-track-gray-100 scrollbar-thumb-slate-400 scrollbar-thumb-rounded-md">
+        <div className='flex justify-center items-start mt-[60px] w-full'>
+          <div className='max-w-[900px] flex flex-col gap-10 items-start justify-start w-full'>
+            <div className='flex flex-col gap-6 w-full'>
+              <div className='flex justify-between items-end mobile:!flex-wrap'>
+                {
+                  projectStateUpdate
+                    ? <div className='mobile:!w-full w-[500px] flex justify-start items-center pt-2'>
+                      <LineSkeleton className='w-[200px] !h-[38px] !rounded-lg' />
+                    </div>
+                    : <input
+                      type="text"
+                      name="title"
+                      value={editTitle || ""}
+                      onChange={updateTitle}
+                      placeholder='Title field should not be empty'
+                      className='placeholder:text-[18px] text-4xl font-bold text-slate-900 truncate text-ellipsis max-w-[500px] mobile:max-w-[250px] border-none rounded-md pl-1' />
+                }
+                <span className='text-xl font-medium text-slate-900'>{moment(created_at).format('MMMM DD, YYYY')}</span>
               </div>
-              {teamTempoData
-                .slice(0, teamLimit ? teamTempoData.length : 11)
-                .map((team: any, index: number) => {
-                  return <TeamTemplate key={index} data={team} callBack={renameTeam} />
-                })}
-              {teamTempoData.slice(0, 11).length === 11 && (
-                <div onClick={() => setTeamLimit(!teamLimit)} css={homeStyle.more}>
-                  <span>Show {teamLimit ? 'less' : 'more'}</span>
-                </div>
-              )}
+              {
+                projectStateUpdate
+                  ? <div className='border border-slate-300 p-5 rounded-md h-[142px] w-full'>
+                    <LineSkeleton className='w-[100%]' />
+                    <LineSkeleton className='w-[80%]' />
+                    <LineSkeleton className='w-[50%]' />
+                    <LineSkeleton className='w-[30%]' />
+                    <LineSkeleton className='w-[20%]' /></div>
+                  : <textarea placeholder='Description field should not be empty' onChange={updateDescription} value={editDescription || ""} name="description" rows={5} className='text-sm text-slate-500 break-words border border-slate-300 p-5 rounded-md' />
+              }
+              <div className='flex w-full items-end justify-end'>
+                <SubmitButton
+                  text="Save"
+                  submitted={onSaveChanges}
+                  isSubmitting={saveChanges}
+                  isDisabled={editDescription?.length === 0 || editTitle?.length === 0}
+                  className="!w-[175px] mobile:!w-full !text-slate-600 !bg-slate-200 hover:!bg-blue-600 hover:!text-slate-50" />
+              </div>
             </div>
-          </div>
 
-          <div className='w-full flex flex-col gap-3'>
-            <h1 className='text-lg font-bold'>Members</h1>
-            <div className='grid grid-cols-4 gap-6 tablet:!grid-cols-2 mobile:!grid-cols-1'>
-              {membersData
-                .slice(0, membersLimit ? membersData.length : 12)
-                .map((member: any, index: number) => {
-                  return <MembersTemplate key={index} data={member} />
-                })}
-              {membersData.slice(0, 12).length === 12 && (
-                <div onClick={() => setMembersLimit(!membersLimit)} css={homeStyle.more}>
-                  <span>Show {membersLimit ? 'less' : 'more'}</span>
+            <div className='w-full flex flex-col gap-3'>
+              <h1 className='text-lg font-bold'>Team</h1>
+              <div className='grid grid-cols-4 gap-6 tablet:!grid-cols-2 mobile:!grid-cols-1'>
+                <div onClick={() => setAddTeamState(!addTeamState)} className='flex items-center gap-2 w-[200px] cursor-pointer'>
+                  <div className='w-[44px] h-[44px] flex justify-center items-center rounded-full border-2 border-dotted border-slate-600'>
+                    <Plus />
+                  </div>
+                  <button className='text-base font-semibold'>Add Team</button>
                 </div>
-              )}
+                {
+                  teamStateUpdate
+                    ? <TeamTemplate data={null} loadingState={teamStateUpdate} />
+                    : teams?.slice(0, teamLimit ? teams?.length : 11)
+                      .map((team: any, index: number) => {
+                        return <TeamTemplate key={index} data={team} callBack={renameTeam} teamCount={teams?.length} />
+                      })
+                }
+                {teamStateUpdate || teams?.slice(0, 12).length === 12 && <SeeMore set={setTeamLimit} what={teamLimit} />}
+              </div>
+            </div>
+
+            <div className='w-full flex flex-col gap-3'>
+              <h1 className='text-lg font-bold'>Members</h1>
+              <div className='grid grid-cols-4 gap-6 tablet:!grid-cols-2 mobile:!grid-cols-1'>
+                {
+                  memberStateUpdate
+                    ? <MembersTemplate data={null} loadingState={memberStateUpdate} />
+                    : members?.slice(0, membersLimit ? members?.length : 12)
+                      .map((member: any, index: number) => {
+                        return <MembersTemplate key={index} data={member} />
+                      })
+                }
+                {memberStateUpdate || members?.slice(0, 13).length === 13 && (<SeeMore set={setMembersLimit} what={membersLimit} />)}
+              </div>
             </div>
           </div>
         </div>
