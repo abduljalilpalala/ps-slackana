c2d5c4c9bbdb33fbd807f0ce9ff28f94f5a7c23a
John Paul Banera <johnpaul.banera@sun-asterisk.com>
2022-10-09 21:23:49 +0800

    [FE]-15 Fetch Project Data from API

---
+++ client/src/shared/jsons/dummyTeamIcon.ts
@@ -0,0 +1,10 @@
+export const dummyTeamIcon = [
+  "/images/team/frontend.png",
+  "/images/team/backend.png",
+  "/images/team/qa.png",
+  "/images/team/softwareqa.png",
+  "/images/team/projectmanager.png",
+  "/images/team/teamlead.png",
+  "/images/team/designer.png",
+  "/images/team/null.png",
+]
