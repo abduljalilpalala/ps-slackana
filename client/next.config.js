6ae47ddaef86d4f8ee55918e6231b3545d2c816c
John Paul Banera <104751512+pong-sun@users.noreply.github.com>
2022-10-09 21:21:28 +0800

    [FE]-13 Integrate Update User Information, Avatar, and Password Functionality (#55)

--- client/next.config.js
+++ client/next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: false,
   swcMinify: true,
   images: {
-    domains: ['images.unsplash.com', 'ca.slack-edge.com']
+    domains: ['images.unsplash.com', 'ca.slack-edge.com', 'localhost']
   },
   env: {
     NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL
