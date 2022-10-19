9f0611ce45dbbdfed3ec20872bb2a0e7fbab8182
John Paul Banera <104751512+johnpaul-sun@users.noreply.github.com>
2022-10-19 10:42:56 +0800

    [226] - [Markup] Create Nudge Component (#102)

---
+++ client/src/utils/nudgeMember.tsx
@@ -0,0 +1,37 @@
+import toast from "react-hot-toast"
+import handleImageError from "~/helpers/handleImageError"
+
+export const nudgeMember = (avatar: string, name: string) => {
+  return toast.custom((t: any) => (
+    <div className={`${t.visible ? 'animate-enter' : 'animate-leave'} max-w-[300px] w-full bg-white shadow-lg rounded-lg pointer-events-auto flex ring-1 ring-black ring-opacity-5`}>
+      <div className="flex-1 w-0 p-4">
+        <div className="flex items-start">
+          <div className="flex-shrink-0 pt-0.5">
+            <img
+              className="h-10 w-10 rounded-full"
+              src={avatar || "/images/avatar.png"}
+              onError={(e) => handleImageError(e, '/images/avatar.png')}
+              alt=""
+            />
+          </div>
+          <div className="ml-3 flex-1">
+            <p className="text-sm font-medium text-gray-900 max-w-[150px] truncate w-full">
+              {name}
+            </p>
+            <p className="mt-1 text-sm text-gray-500">
+              Nudges you
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex border-l border-gray-200">
+        <button
+          onClick={() => toast.dismiss(t.id)}
+          className="w-full border border-transparent rounded-none rounded-r-lg p-4 flex items-center justify-center text-sm font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+        >
+          Close
+        </button>
+      </div>
+    </div>
+  ))
+}
