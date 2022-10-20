def5bdeaf44cfa13df27e5e9a1f6ac97b95e500f
abduljalilpalala <abduljalil.palala@sun-asterisk.com>
2022-10-20 23:12:16 +0800

    [618] - [FE] Add Environment variables at next.config.js for Pusher JS

--- client/next.config.js
+++ client/next.config.js
@@ -6,7 +6,9 @@ const nextConfig = {
     domains: ['images.unsplash.com', 'ca.slack-edge.com', 'localhost']
   },
   env: {
-    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL
+    NEXT_PUBLIC_BACKEND_URL: process.env.NEXT_PUBLIC_BACKEND_URL,
+    NEXT_PUBLIC_PUSHER_KEY: process.env.NEXT_PUBLIC_PUSHER_KEY,
+    NEXT_PUBLIC_PUSHER_CLUSTER: process.env.NEXT_PUBLIC_PUSHER_CLUSTER
   }
 }
 
