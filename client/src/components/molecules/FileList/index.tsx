50f946e6efc143394fabe487b6dc2bae548724e9
Marc Jermaine Pontiveros <marcjermaine.pontiveros@sun-asterisk.com>
2022-11-04 17:40:18 +0800

    [269] - [BugTask] When renaming the file with existing filename, ask the user to continue by adding a number suffix or cancel

--- client/src/components/molecules/FileList/index.tsx
+++ client/src/components/molecules/FileList/index.tsx
@@ -56,38 +56,40 @@ const FileList: FC<Props> = (props): JSX.Element => {
   ) : length === 0 ? (
     <BlankTable />
   ) : (
-    <table className="w-full divide-y divide-slate-300 border-t border-slate-300 text-left text-sm leading-normal">
-      <TableHead />
-      <tbody className="divide-y divide-slate-300 text-sm text-slate-600">
-        {isOpen && (
-          /*
-           * This is the modal for Edit Filename that
-           * will eventually render if `isOpen` props is true
-           * P.S: `isOpen` is necessary in order to refresh the state stored
-           *       in the variable
-           */
-          <EditFilenameDialog
-            isOpen={isOpen}
-            filename={filename}
-            closeModal={handleOpenEditModal}
-            actions={{ handleUpdateFilename }}
-          />
-        )}
-        {props.files?.map((file, i) => (
-          /*
-           * This will rendered actual data
-           * <tr>
-           *  <td>{data goes here}</td>
-           * </tr>
-           */
-          <FileItem
-            key={i}
-            file={file}
-            actions={{ handleOpenEditModal, handleDeleteFile, handleDownloadFile }}
-          />
-        ))}
-      </tbody>
-    </table>
+    <>
+      {isOpen && (
+        /*
+         * This is the modal for Edit Filename that
+         * will eventually render if `isOpen` props is true
+         * P.S: `isOpen` is necessary in order to refresh the state stored
+         *       in the variable
+         */
+        <EditFilenameDialog
+          isOpen={isOpen}
+          filename={filename}
+          closeModal={handleOpenEditModal}
+          actions={{ handleUpdateFilename }}
+        />
+      )}
+      <table className="w-full divide-y divide-slate-300 border-t border-slate-300 text-left text-sm leading-normal">
+        <TableHead />
+        <tbody className="divide-y divide-slate-300 text-sm text-slate-600">
+          {props.files?.map((file, i) => (
+            /*
+             * This will rendered actual data
+             * <tr>
+             *  <td>{data goes here}</td>
+             * </tr>
+             */
+            <FileItem
+              key={i}
+              file={file}
+              actions={{ handleOpenEditModal, handleDeleteFile, handleDownloadFile }}
+            />
+          ))}
+        </tbody>
+      </table>
+    </>
   )
 }
 
