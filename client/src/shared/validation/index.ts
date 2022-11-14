57d846d23f37b9ccb2d5ab98c31c73694ababf53
acatzk <joshuaimalay@gmail.com>
2022-11-14 20:59:58 +0800

    [881] - [BugTask] The user changes the password, invalid input, the error message is displayed and the setting modal is closed, the error message is still displayed

--- client/src/shared/validation/index.ts
+++ client/src/shared/validation/index.ts
@@ -20,3 +20,15 @@ export const SignUpFormSchema = Yup.object().shape({
     .max(12, 'Password cannot exceed more than 12 characters')
     .oneOf([Yup.ref('password')], 'Passwords do not match')
 })
+
+export const SecurityFormSchema = Yup.object().shape({
+  current_password: Yup.string().required('Current password is required'),
+  new_password: Yup.string()
+    .required('Password is required')
+    .min(9, 'Password length should be at least 9 characters')
+    .max(15, 'Password cannot exceed more than 15 characters'),
+  confirm_password: Yup.string()
+    .required('Confirm Password is required')
+    .max(15, 'Password cannot exceed more than 15 characters')
+    .oneOf([Yup.ref('new_password')], 'Passwords do not match')
+})
