033dfc45ff1df91adaeb8daf20c4457ac0e7286c
Joshua Galit <108642414+jsvelte@users.noreply.github.com>
2022-11-16 14:37:30 +0800

    [252] - [Markup] - Modify Home Page Design (#188)
    Co-authored-by: acatzk <joshuaimalay@gmail.com>

--- client/src/components/organisms/DropDownFilter/index.tsx
+++ client/src/components/organisms/DropDownFilter/index.tsx
@@ -1,50 +1,49 @@
 import { Menu, Transition } from '@headlessui/react'
 import { Fragment, useState } from 'react'
 import { ProjectStatus } from '~/shared/icons/ProjectStatus'
-import { projectStatusData } from '~/shared/jsons/projectStatusData';
+import { projectStatusData } from '~/shared/jsons/projectStatusData'
 import { styles as homeStyle } from '~/shared/twin/home-content.style'
-import statusChecker from '~/utils/statusChecker';
-import { setFilter, filterProjects } from "~/redux/project/projectSlice";
+import statusChecker from '~/utils/statusChecker'
+import { setFilter, filterProjects } from '~/redux/project/projectSlice'
 import { useAppDispatch, useAppSelector } from '~/hooks/reduxSelector'
 
 export default function DropDownFilter({ children, callback }: any) {
-  const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch()
   const { filter } = useAppSelector((state) => state.project)
 
-  const [selected, setSelected] = useState<number>(filter);
+  const [selected, setSelected] = useState<number>(filter)
 
   const onSelect = (index: number): void => {
-    dispatch(setFilter(index));
-    dispatch(filterProjects());
-    setSelected(index);
+    dispatch(setFilter(index))
+    dispatch(filterProjects())
+    setSelected(index)
   }
 
   const dropDownMenu = projectStatusData.map((status: string, index: number) => {
     return (
-      <div className="px-1 py-1 " key={index}>
+      <div key={index}>
         <Menu.Item>
           {({ active }) => (
             <button
               onClick={() => onSelect(index)}
-              className={`${active || selected === index ? 'bg-slate-500 text-white' : 'text-gray-900'
-                } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
+              className={`${
+                active || selected === index ? 'bg-slate-100' : 'text-gray-900'
+              } group flex w-full items-center px-2 py-2 text-sm`}
             >
-              {<ProjectStatus status={statusChecker(status)} className='mx-2 mr-3' />}
+              {<ProjectStatus status={statusChecker(status)} className="mx-2 mr-3" />}
               {status}
             </button>
           )}
         </Menu.Item>
       </div>
-    );
-  });
+    )
+  })
 
   return (
-    <div className='z-10'>
+    <div className="z-10">
       <Menu as="div" className="relative inline-block text-left">
         <div>
-          <Menu.Button >
-            {children}
-          </Menu.Button>
+          <Menu.Button>{children}</Menu.Button>
         </div>
         <Transition
           as={Fragment}
@@ -55,7 +54,10 @@ export default function DropDownFilter({ children, callback }: any) {
           leaveFrom="transform opacity-100 scale-100"
           leaveTo="transform opacity-0 scale-95"
         >
-          <Menu.Items css={homeStyle.dropDownMenu}>
+          <Menu.Items
+            css={homeStyle.dropDownMenu}
+            className="py-1 transition duration-75 ease-in-out"
+          >
             {dropDownMenu}
           </Menu.Items>
         </Transition>
--- client/src/shared/twin/home-content.style.ts
+++ client/src/shared/twin/home-content.style.ts
@@ -1,16 +1,17 @@
 import tw from 'twin.macro'
 
-const slate_50 = '#f8fafc';
-const slate_100 = '#F1F5F9';
-const slate_200 = '#e2e8f0';
-const slate_300 = '#cbd5e1';
-const slate_400 = '#94a3b8';
-const slate_500 = '#64748b';
-const slate_600 = '#475569';
-const slate_700 = '#334155';
-const slate_800 = '#1e293b';
-const slate_900 = '#0f172a';
-export const scrollbar = 'scrollbar-thin scrollbar-thumb-slate-400 scrollbar-thumb-rounded-md scrollbar-track-gray-100 border-x'
+const slate_50 = '#f8fafc'
+const slate_100 = '#F1F5F9'
+const slate_200 = '#e2e8f0'
+const slate_300 = '#cbd5e1'
+const slate_400 = '#94a3b8'
+const slate_500 = '#64748b'
+const slate_600 = '#475569'
+const slate_700 = '#334155'
+const slate_800 = '#1e293b'
+const slate_900 = '#0f172a'
+export const scrollbar =
+  'scrollbar-thin scrollbar-thumb-slate-400 scrollbar-thumb-rounded-md scrollbar-track-gray-100 border-x'
 
 export const styles = {
   home: [tw`flex items-center justify-center flex-col gap-16`],
@@ -18,10 +19,20 @@ export const styles = {
   body: [tw`px-6 py-3 rounded-t-lg border border-[${slate_300}]`],
   bodyHeader: [tw`flex flex-row justify-between content-between`],
   addProjectDiv: [tw`flex flex-row items-center justify-center gap-3`],
-  addProjectButton: [tw`hover:bg-[${slate_100}] cursor-pointer w-12 h-12 flex items-center justify-center rounded-lg border-dotted border border-[${slate_400}]`],
-  dropDown: [tw`hover:bg-[${slate_100}] cursor-pointer w-12 h-12 flex items-center justify-center rounded-lg border border-[${slate_500}]`],
-  container: [tw`z-10 border-b rounded-b-lg border-[${slate_300}] mobile:grid-cols-1 tablet:grid-cols-2 px-7 py-10 grid grid-cols-3 gap-6 h-px-400 overflow-y-scroll`],
-  more: [tw`text-[${slate_600}] hover:text-[${slate_400}] ease-in-out duration-300 font-medium cursor-pointer flex justify-start items-center`],
+  addProjectButton: [
+    tw`hover:bg-[${slate_100}] cursor-pointer w-12 h-12 flex items-center justify-center rounded-lg border-dotted border border-[${slate_400}]`
+  ],
+  dropDown: [
+    tw`hover:bg-[${slate_100}] cursor-pointer w-12 h-12 flex items-center justify-center rounded-lg border border-[${slate_500}]`
+  ],
+  container: [
+    tw`z-10 border-b rounded-b-lg border-[${slate_300}] mobile:grid-cols-1 tablet:grid-cols-2 px-7 py-10 grid grid-cols-3 gap-6 h-px-400 overflow-y-scroll`
+  ],
+  more: [
+    tw`text-[${slate_600}] hover:text-[${slate_400}] ease-in-out duration-300 font-medium cursor-pointer flex justify-start items-center`
+  ],
   tags: [tw`flex flex-col justify-start items-start`],
-  dropDownMenu: [tw`absolute z-20 right-0 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`]
+  dropDownMenu: [
+    tw`absolute z-20 right-0 w-48 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none`
+  ]
 }
