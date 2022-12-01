c1310e488942d2c497490867b517bc30c5f04f22
Rogelio John Oliverio <110364637+rogeliojohnoliverio@users.noreply.github.com>
2022-12-01 11:05:58 +0800

    [346] - [FE/BE] Setup Docker (#203)

--- client/next.config.js
+++ client/next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    apiUrl: process.env.BACKEND_URL
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    apiUrl: process.env.NEXT_PUBLIC_BACKEND_URL
+  },
   reactStrictMode: false,
   swcMinify: true,
   images: {
--- client/src/shared/lib/axios.ts
+++ client/src/shared/lib/axios.ts
@@ -1,10 +1,11 @@
 import Axios from 'axios'
 import { hasCookie, getCookie } from 'cookies-next'
+import getConfig from 'next/config'
 
 const token = getCookie('token')
-
+const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
 export const axios: any = Axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
+  baseURL: serverRuntimeConfig.apiUrl || publicRuntimeConfig.apiUrl,
   headers: {
     Accept: 'application/json',
     Authorization: `Bearer ${token}`
